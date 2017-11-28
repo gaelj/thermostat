b0526ab67159675fe6ae61c3b71078a47fe57abf
gaelj <gaeljames@gmail.com>
2017-11-28 19:16:02 +0100

    Comments

--- Thermostat.ino
+++ Thermostat.ino
@@ -76,26 +76,33 @@ bool settingsError = false;
 *
 */
 void DrawDisplay() {
+    // clear screen
     oled.clrscr();
     delay(5);
+
+    // sensor temperature
     oled.setFont(zuno_font_numbers16);
     oled.gotoXY(13, 0);
     oled.fixPrint((long)(SENSOR.GetTemperature() * 10), 1);
 
+    // setpoint temperature
     oled.gotoXY(13, 4);
     oled.fixPrint((long)(SETTINGS.GetSetPoint(MODE.CurrentThermostatMode) * 10), 1);
 
+    // boiler state
     if (BOILER.GetBoilerState()) {
         oled.gotoXY(80, 4);
         oled.writeData(flame_data);
     }
+
+    // thermostat mode
     oled.gotoXY(80, 0);
     switch (THERM.GetMode()) {
-    case Day: oled.writeData(sun_data); break;
-    case Night: oled.writeData(moon_data); break;
-    case Absent: oled.writeData(absent_data); break;
-    case Frost: oled.writeData(snow_data); break;
-    case Warm: oled.writeData(hot_data); break;
+        case Day: oled.writeData(sun_data); break;
+        case Night: oled.writeData(moon_data); break;
+        case Absent: oled.writeData(absent_data); break;
+        case Frost: oled.writeData(snow_data); break;
+        case Warm: oled.writeData(hot_data); break;
     }
 }
 
