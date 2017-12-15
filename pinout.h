2c80baa146438f22986d7158dcc1970c11055684
gaelj <gaeljames@gmail.com>
2017-12-15 02:37:57 +0100

    Fix LED pinout

--- pinout.h
+++ pinout.h
@@ -9,16 +9,16 @@
 #define PIN_BUTTON1             18
 #define PIN_BUTTON2             17
 
-#define PIN_LED_R1              16
-#define PIN_LED_G1              15
-#define PIN_LED_B1              14
+#define PIN_LED_R1              6
+#define PIN_LED_G1              7
+#define PIN_LED_B1              8
 
-#define PIN_LED_R2              21
-#define PIN_LED_G2              20
-#define PIN_LED_B2              19
+#define PIN_LED_R2              16
+#define PIN_LED_G2              15
+#define PIN_LED_B2              14
 
-#define PIN_LED_R3              6
-#define PIN_LED_G3              7
-#define PIN_LED_B3              8
+#define PIN_LED_R3              21
+#define PIN_LED_G3              20
+#define PIN_LED_B3              19
 
 #endif /* THERMOSTAT_PINOUT_H_ */
