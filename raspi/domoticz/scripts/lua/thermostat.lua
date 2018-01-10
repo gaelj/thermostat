332065eac33489f488ad76576ff385d294e18a47
gaelj <gaeljames@gmail.com>
2018-01-11 00:39:47 +0100

    Update lua script

--- raspi/domoticz/scripts/lua/thermostat.lua
+++ raspi/domoticz/scripts/lua/thermostat.lua
@@ -22,36 +22,51 @@
 --
 -- Based on your logic, fill the commandArray with device commands. Device name is case sensitive. 
 --
+
+
+-- Handles communication with the custom Zwave thermostat
+
+
 commandArray = {}
 
-print ("=== Start of switches script ===")
+local sensorwu = 'THB Potelles' 
 
-p="(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)"
-offset=os.time()-os.time(os.date("!*t"))
+currentMode = 0
+boilerValue = nil
+extTemp = nil
+extHum = nil
+extPres = nil
 
-newLevel = 0
-oldCommandId = 0
-oldValue = 0
-oldSetpoint = 0
-extTemp1 = 0
-extTemp2 = 0
-
-t1 = 'Thermostat Hors Gel'
-t2 = 'Thermostat Parti'
-t3 = 'Thermostat Nuit'
-t4 = 'Thermostat Jour'
-t5 = 'Thermostat Confort'
 
 -- Radiator setpoints
-sp1Name    = 'Setpoint bureau etage'
-sp2Name    = 'Setpoint palier'
-sp3Name    = 'Setpoint chambre'
-spR1       = 96
-spR2       = 23
-spR3       = 20
-Tb         = nil
-Tp         = nil
-Tc         = nil
+RadiatorTempSensors = {['Temp vanne bureau'] = 0, ['Temp vanne palier'] = 0, ['Temp vanne chambre'] = 0, ['Temp salon canape'] = 0, ['Temp salon tele'] = 0, ['Temp salon rue'] = 0}
+
+ModeValues = {["Off"] = 0, ["Frost"] = 1, ["Absent"] = 2, ["Night"] = 3, ["Day"] = 4, ["Comfort"] = 5, ["DayFloor1"] = 6, ["ComfortFloor1"] = 7, ["DayFloor2"] = 8, ["ComfortFloor2"] = 9 }
+ModeStrings = {[0] = "Off", [1] = "Frost", [2] = "Absent", [3] = "Night", [4] = "Day", [5] = "Comfort", [6] = "DayFloor1", [7] = "ComfortFloor1", [8] = "DayFloor2", [9] = "ComfortFloor2" }
+
+RadiatorSetPoints1 = { ["Name"] = 'Setpoint bureau etage', ["Sensor"] = 'Temp vanne bureau',  ["id"] = 96,  ["Temperatures"] = { ["Frost"] = 5,  ["Absent"] = 13,  ["Night"] = 15,  ["Day"] = 20,    ["Comfort"] = 22 }}
+RadiatorSetPoints2 = { ["Name"] = 'Setpoint palier',       ["Sensor"] = 'Temp vanne palier',  ["id"] = 23,  ["Temperatures"] = { ["Frost"] = 5,  ["Absent"] = 13,  ["Night"] = 15,  ["Day"] = 17.5,  ["Comfort"] = 18 }}
+RadiatorSetPoints3 = { ["Name"] = 'Setpoint chambre',      ["Sensor"] = 'Temp vanne chambre', ["id"] = 20,  ["Temperatures"] = { ["Frost"] = 5,  ["Absent"] = 13,  ["Night"] = 16,  ["Day"] = 16,    ["Comfort"] = 16 }}
+RadiatorSetPoints4 = { ["Name"] = 'Setpoint salon canape', ["Sensor"] = 'Temp salon canape',  ["id"] = 276, ["Temperatures"] = { ["Frost"] = 5,  ["Absent"] = 13,  ["Night"] = 15,  ["Day"] = 19,    ["Comfort"] = 21 }}
+RadiatorSetPoints5 = { ["Name"] = 'Setpoint salon tele',   ["Sensor"] = 'Temp salon tele',    ["id"] = 279, ["Temperatures"] = { ["Frost"] = 5,  ["Absent"] = 13,  ["Night"] = 15,  ["Day"] = 19,    ["Comfort"] = 21 }}
+RadiatorSetPoints6 = { ["Name"] = 'Setpoint salon rue',    ["Sensor"] = 'Temp salon rue',     ["id"] = 282, ["Temperatures"] = { ["Frost"] = 5,  ["Absent"] = 13,  ["Night"] = 14,  ["Day"] = 17,    ["Comfort"] = 19 }}
+
+RadiatorSetPoints = { RadiatorSetPoints1, RadiatorSetPoints2, RadiatorSetPoints3, RadiatorSetPoints4, RadiatorSetPoints5, RadiatorSetPoints6 }
+
+for i, r in ipairs(RadiatorSetPoints) do
+    down = i > 3
+    if down then
+        r["Temperatures"]["DayFloor1"] = r["Temperatures"]["Day"]
+        r["Temperatures"]["ComfortFloor1"] = r["Temperatures"]["Comfort"]
+        r["Temperatures"]["DayFloor2"] = r["Temperatures"]["Night"]
+        r["Temperatures"]["ComfortFloor2"] = r["Temperatures"]["Night"]
+    else
+        r["Temperatures"]["DayFloor1"] = r["Temperatures"]["Night"]
+        r["Temperatures"]["ComfortFloor1"] = r["Temperatures"]["Night"]
+        r["Temperatures"]["DayFloor2"] = r["Temperatures"]["Day"]
+        r["Temperatures"]["ComfortFloor2"] = r["Temperatures"]["Comfort"]
+    end
+end
 
 function GetSliderValue(valueRaw)
     local valueStr = ''
@@ -63,252 +78,147 @@ function GetSliderValue(valueRaw)
     else
         value = tonumber(valueStr)
         if value == nil then
-            valueStr = string.gsub(string.sub(valueRaw, 0, -2), "%s", "")
             valueStr = string.gsub(string.sub(valueRaw, 0, -2), "Set Level:", "")
+            valueStr = string.gsub(valueStr, "%s", "")
             value = tonumber(valueStr)
         end
     end
     return value
 end
 
-function GetSetPointValue(valueRaw)
-    local value = 0
-    if valueRaw == "Off" then
-        value = 0
-    elseif valueRaw == "Frost" or valueRaw == t1 then
-        value = 10
-    elseif valueRaw == "Absent" or valueRaw == t2 then
-        value = 20
-    elseif valueRaw == "Night" or valueRaw == t3 then
-        value = 30
-    elseif valueRaw == "Day" or valueRaw == t4 then
-        value = 40
-    elseif valueRaw == "Comfort" or valueRaw == t5 then
-        value = 50
-    end
-    return value
-end
-
-function GetThermostatSetPoint(valueRaw)
-    local value = "Off"
-    if valueRaw < 10 then
-        value = "Off"
-    elseif valueRaw < 20 then
-        value = "Frost"
-    elseif valueRaw < 30 then
-        value = "Absent"
-    elseif valueRaw < 40 then
-        value = "Night" 
-    elseif valueRaw < 50 then
-        value = "Day"
-    elseif valueRaw < 60 then
-        value = "Comfort"
-    end
-    return value
-end
-
-function DeviceIsAThermostatSwitch(deviceName)
-    return (deviceName == t1 or deviceName == t2 or deviceName == t3 or deviceName == t4 or deviceName == t5)
-end
-
-function TurnOffOtherSwitches(keepName)
-    print("TurnOffOtherSwitches: " .. keepName)
-    if keepName ~= t1 then
-       commandArray[t1] = "Off"
-    end
-    if keepName ~= t2 then
-       commandArray[t2] = "Off"
-    end
-    if keepName ~= t3 then
-       commandArray[t3] = "Off"
-    end
-    if keepName ~= t4 then
-       commandArray[t4] = "Off"
-    end
-    if keepName ~= t5 then
-       commandArray[t5] = "Off"
+function SetThermostatMode(newMode)
+    print("newMode " .. newMode)
+    print("currentMode " .. currentMode)
+    if (newMode > 0 and currentMode ~= newMode) then
+        print("The thermostat mode changed to " .. newMode .. ": " .. ModeStrings[newMode])
+        commandArray['Thermostat Setpoint'] = 'Set Level ' .. (newMode * 10)
     end
 end
 
-function TurnOnSwitch(switchName)
-    print("Turn on switch: " .. switchName)
-    commandArray[switchName] = "On"
-end
-
-function SetRadiators(newSetpoint)
-    -- Send new setpoints to radiators if mode has changed
-    if (newSetpoint > 0 and oldSetpoint ~= newSetpoint) then
-        if Tb ~= nil then
-            print("Sending new setpoints to radiators")
-            commandArray[#commandArray + 1] = {['UpdateDevice']=spR1..'|0|'..tostring(Tb)}
-            commandArray[#commandArray + 1] = {['UpdateDevice']=spR2..'|0|'..tostring(Tp)}
-            commandArray[#commandArray + 1] = {['UpdateDevice']=spR3..'|0|'..tostring(Tc)}
+function SetRadiators(modeId)
+    if modeId > 0 then
+        print("Sending new setpoints to radiators")
+        for i, r in ipairs(RadiatorSetPoints) do
+            commandArray[#commandArray + 1] = {['UpdateDevice']=r["id"] .. '|0|' .. tostring(r["Temperatures"][ModeStrings[modeId]])}
         end
     end
 end
 
-function SetThermostatSetpoint(newSetpoint)
-    print("newSetpoint " .. newSetpoint)
-    print("oldSetpoint " .. oldSetpoint)
-    if (newSetpoint > 0 and oldSetpoint ~= newSetpoint) then
-        print("The thermostat switch changed to " .. newLevel .. ": " .. GetThermostatSetPoint(newLevel))
-        commandArray['Thermostat Setpoint'] = 'Set Level ' .. newSetpoint
+function EnforceMinMax(val)
+    if val < 0 then
+        val = 0
+    elseif val > 99 then
+        val = 99
+    end
+    return val
+end
+
+-- Get current values
+for deviceName, deviceValue in pairs(otherdevices) do
+    if (deviceName == 'Thermostat Setpoint') then
+        currentMode = ModeValues[deviceValue]
+        
+    elseif (deviceName == sensorwu) then
+        sWeatherTemp, sWeatherHumidity, sHumFeelsLike, sWeatherPressure = deviceValue:match("([^;]+);([^;]+);([^;]+);([^;]+);([^;]+)")
+        extTemp = EnforceMinMax(math.floor((sWeatherTemp * 2) + 50))
+        extHum = EnforceMinMax(math.floor(sWeatherHumidity))
+        extPres = EnforceMinMax(math.floor(sWeatherPressure - 950))
+    
+    elseif RadiatorTempSensors[deviceName] ~= nil then
+        RadiatorTempSensors[deviceName] = deviceValue
+    elseif deviceName == "Z_Boiler" then
+        boilerValue = GetSliderValue(deviceValue)
     end
 end
 
-
-
--- commands
-
-No_command                 = 0
--- Set commands: PC sends value to Zuno / Zuno requests value from PC
-Get_Mode                   = 1
-
-Get_Radiator_Count         = 2
-Get_ActiveRadiator         = 3
-
-Get_RadiatorSetpoint1      = 4
-Get_RadiatorSetpoint2      = 5
-
-Get_RadiatorTemperature1   = 6
-Get_RadiatorTemperature2   = 7
-
-Get_ExteriorTemperature1   = 8
-Get_ExteriorTemperature2   = 9
-
-Get_ExteriorHumidity1      = 10
-Get_ExteriorHumidity2      = 11
-
-Get_ExteriorPressure1      = 12
-Get_ExteriorPressure2      = 13
-
--- Set commands: Zuno sends value to PC / PC requests value from Zuno
-Rep_Mode                    = 14
-
-Rep_Radiator_Count         = 15
-Rep_ActiveRadiator         = 16
-
-Rep_RadiatorSetpoint1      = 17
-Rep_RadiatorSetpoint2      = 18
-
-Rep_RadiatorTemperature1   = 19
-Rep_RadiatorTemperature2   = 20
-
-Rep_ExteriorTemperature1   = 21
-Rep_ExteriorTemperature2   = 22
-
-Rep_ExteriorHumidity1      = 23
-Rep_ExteriorHumidity2      = 24
-
-Rep_ExteriorPressure1      = 25
-Rep_ExteriorPressure2      = 26
-
-Set_mode                   = 27
-
-function SendCommand(commandId, value)
-    print("SEND COMMAND: " .. commandId .. ": " .. value)
-    commandArray['ZRX command'] = "Set Level " .. commandId
-    commandArray['ZRX value'] = "Set Level " .. value
+-- Hysteresis
+boilerNeeded = 0
+hysteresis = 0.5
+for i, r in ipairs(RadiatorSetPoints) do
+    setPoint = tonumber(r["Temperatures"][ModeStrings[currentMode]])
+    temperature = tonumber(RadiatorTempSensors[r["Sensor"]])
+    if uservariables['IsInHeatingCycle'] == 0 and (temperature < setPoint - hysteresis) then
+        boilerNeeded = 1
+    elseif uservariables['IsInHeatingCycle'] == 1 and (temperature < setPoint) then
+        boilerNeeded = 1
+    end
+end
+--print('boilerNeeded: ' .. tostring(boilerNeeded) .. ' boilerValue: ' .. boilerValue .. ' IsInHeatingCycle: ' .. tostring(uservariables['IsInHeatingCycle']))
+if boilerNeeded == 1 and boilerValue < 99 then
+    print("$$$$$$$$$$$$ BOILER ON REQ")
+    commandArray["Z_Boiler"] = "Set Level On"
+    uservariables['IsInHeatingCycle'] = 1
+elseif boilerNeeded == 0 and boilerValue > 0 then
+    print("$$$$$$$$$$$$ BOILER OFF REQ")
+    commandArray["Z_Boiler"] = "Set Level Off"
+    uservariables['IsInHeatingCycle'] = 0
 end
 
-
--- get current values
-for deviceName, deviceValue in pairs(otherdevices) do
-    if (deviceName == 'ZTX command') then
-        oldCommandId = GetSliderValue(deviceValue)
-        
-    elseif (deviceName == 'ZTX value') then
-        oldValue = GetSliderValue(deviceValue)
-        
-    elseif (deviceName == 'Thermostat Setpoint') then
-        oldSetpoint = GetSetPointValue(deviceValue)
-        
-    elseif (deviceName == 'Temperature Potelles') then
-        extTemp1 = math.floor(deviceValue)
-        extTemp2 = math.floor((deviceValue - extTemp1) * 100)
+-- Apply values to sliders if needed
+if extTemp ~= nil then
+    for deviceName, deviceValue in pairs(otherdevices) do
+        if deviceName == "Z_ExteriorTemperature" then
+            if GetSliderValue(deviceValue) ~= extTemp then
+                commandArray[deviceName] = "Set Level " .. extTemp
+            end
+            
+        elseif deviceName == "Z_ExteriorHumidity" then
+            if GetSliderValue(deviceValue) ~= extHum then
+                commandArray[deviceName] = "Set Level " .. extHum
+            end
+            
+        elseif deviceName == "Z_ExteriorPressure" then
+            if GetSliderValue(deviceValue) ~= extPres then
+                commandArray[deviceName] = "Set Level " .. extPres
+            end
+        end
     end
 end
 
 -- Loop through all the changed devices sent by the trigger
 if devicechanged ~= nil then
     for deviceName, deviceValue in pairs(devicechanged) do
-        
-        print("Devices changed: " .. deviceName .. " - new value: " .. deviceValue)
-        
+        --print("Devices changed: " .. deviceName .. " - new value: " .. deviceValue)
+
         if (deviceName == 'Thermostat Setpoint') then
-            newLevel = GetSetPointValue(deviceValue)
+            print("Thermostat deviceValue:" .. deviceValue)
+            newLevel = ModeValues[deviceValue]
             print("Thermostat Setpoint newLevel:" .. newLevel)
-            
-            if (newLevel >= 10 and newLevel < 20) then
-               TurnOnSwitch(t1)
-               Tb = 5.0
-               Tp = 5.0
-               Tc = 5.0
-            elseif (newLevel >= 20 and newLevel < 30) then
-               TurnOnSwitch(t2)
-               Tb = 15.0
-               Tp = 15.0
-               Tc = 15.0
-            elseif (newLevel >= 30 and newLevel < 40) then
-               TurnOnSwitch(t3)
-               Tb = 15.0
-               Tp = 15.0
-               Tc = 16.0
-            elseif (newLevel >= 40 and newLevel < 50) then
-               TurnOnSwitch(t4)
-               Tb = 20.0
-               Tp = 17.5
-               Tc = 16.0
-            elseif (newLevel >= 50 and newLevel < 60) then
-               TurnOnSwitch(t5)
-               Tb = 22.0
-               Tp = 19.0
-               Tc = 16.0
-            else
-               print("Set thermostat & radiators to undefined !")
-            end
+            commandArray['Z_Mode'] = 'Set Level ' .. newLevel
             SetRadiators(newLevel)
-            if (newLevel > 0 and oldSetpoint ~= newLevel) then
-                SendCommand(Rep_Mode, newLevel)
-            end
-
-        elseif (deviceValue == "On" and DeviceIsAThermostatSwitch(deviceName)) then
-            print("DeviceIsAThermostatSwitch: " .. deviceName)
-            TurnOffOtherSwitches(deviceName)
-            SetThermostatSetpoint(GetSetPointValue(deviceName))
             
-        elseif deviceName == "ZTX command" then
-            -- Zuno pull
-            commandId = GetSliderValue(deviceValue)
-            if commandId == Get_Mode then
-                print("ZUNO Pull ! commandId " .. commandId)
-                SendCommand(Rep_Mode, oldSetpoint)
-            elseif commandId == Get_ExteriorTemperature1 then
-                print("ZUNO Pull ! extTemp1 " .. commandId)
-                SendCommand(Rep_ExteriorTemperature1, extTemp1 + 50)
-            elseif commandId == Get_ExteriorTemperature2 then
-                print("ZUNO Pull ! extTemp2 " .. commandId)
-                SendCommand(Rep_ExteriorTemperature2, extTemp2)
-            elseif commandId == No_command then
-                print("No command !")
-            end
-
-        elseif oldCommandId > No_command and deviceName == "ZTX value" then
-            -- Zuno push
+        elseif deviceName == "sensorwu" then
+            commandArray['Z_ExteriorTemperature'] = "Set Level " .. extTemp
+            commandArray['Z_ExteriorHumidity'] = "Set Level " .. extHum
+            commandArray['Z_ExteriorPressure'] = "Set Level " .. extPres
+    
+        elseif deviceName == "Z_Mode" then
             deviceValue = GetSliderValue(deviceValue)
-            if oldCommandId == Set_Mode and deviceValue > 0 then
-                print("ZUNO Push ! Set_Mode")
-                print("deviceValue: " .. deviceValue)
-                print("RECEIVE COMMAND: Get_Mode (" .. Get_Mode .. ") " .. deviceValue)
+            if (deviceValue == 0 or deviceValue >= 99) and currentMode ~= 0 then
+                commandArray[deviceName] = "Set Level " .. currentMode
+            elseif deviceValue > 0 then
+                SetThermostatMode(deviceValue);
+            end
                 
-                SetThermostatSetpoint(deviceValue)
-                commandArray['ZTX value'] = "Set Level 0"
+        elseif deviceName == "Z_ExteriorTemperature" then
+            if deviceValue == 0 and extTemp ~= nil then
+                commandArray[deviceName] = "Set Level " .. extTemp
+            end
+            
+        elseif deviceName == "Z_ExteriorHumidity" then
+            if deviceValue == 0 and extHum ~= nil then
+                commandArray[deviceName] = "Set Level " .. extHum
+            end
+            
+        elseif deviceName == "Z_ExteriorPressure" then
+            if deviceValue == 0 and extPres ~= nil then
+                commandArray[deviceName] = "Set Level " .. extPres
             end
 
         --elseif deviceName == "Thermostat command" and deviceValue == 14 then
         --    --commandArray['Thermostat command'] = "Set Level 1"
-        --    commandArray['Thermostat value'] = "Set Level " .. oldSetpoint
-        --    print("Zuno set Thermostat Mode on Domoticz:" .. oldSetpoint)
+        --    commandArray['Thermostat value'] = "Set Level " .. currentMode
+        --    print("Zuno set Thermostat Mode on Domoticz:" .. currentMode)
         end
     end
 end
@@ -323,6 +233,6 @@ end
 --        end
 --    end
 -- end
-print ("=== End of switches script ===")
 
+commandArray['Variable:IsInHeatingCycle'] = tostring(uservariables['IsInHeatingCycle'])
 return commandArray
